6eac3e7bda52a9947c1a6423562a6a670bcec696
Lemon01202 <52125305+Lemon01202@users.noreply.github.com>
2021-10-07 01:01:38 +0300

    restyling cards

--- src/components/Products/ProductComponent.jsx
+++ src/components/Products/ProductComponent.jsx
@@ -24,14 +24,45 @@ const ProductComponent = () => {
 		<div className='productList'>
 			<div className='productCards'>
 				{product.map((p, index) => (
-					<div key={index} className='productCard'>
-						<div className='productCardImage'><img alt={p.title} src={p.image} /></div>
-						<div className='productCardContent'>
-							<div className=''>Header: {p.title}</div><hr />
-							<div className=''>Meta price: {p.price}</div><hr />
-							<div className=''>Meta: {p.category}</div>
+					<section key={p.id} class="row">
+						<div class="container-item">
+							<div class="item">
+								<img className='itemImage' src={p.image} />
+								<div class="item-overlay">
+									<a href="#" class="item-button play"><i class="play"></i></a>
+									<a href="#" class="item-button share share-btn"><i class="play"></i></a>
+									<div class="sale-tag"><span>SALE</span></div>
+								</div>
+								<div class="item-content">
+									<div class="item-top-content">
+										<div class="item-top-content-inner">
+											<div class="item-product">
+												<div class="item-top-title">
+													<h2>{p.title}</h2>
+													<p class="subdescription">
+														{p.category}
+													</p>
+												</div>
+											</div>
+											<div class="item-product-price">
+												<span class="price-num">{p.price}$</span>
+												{/* <p class="subdescription">$36</p> */}
+												{/* <div class="old-price"></div> */}
+											</div>
+										</div>
+									</div>
+									<div class="item-add-content">
+										<div class="item-add-content-inner">
+
+											<div class="section">
+												<a href="#" class="btn buy expand">Buy now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+					</section>
 				))}
 			</div>
 		</div>
